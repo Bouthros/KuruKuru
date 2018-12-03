126fcc9b3e3675af83677b3ae1e361fb477235b0
pierre <plenost@outlook.com>
2018-12-03 22:12:12 +0100

    finished Magnet script

--- KuruFork/Assets/Script/MagnetScript.cs
+++ KuruFork/Assets/Script/MagnetScript.cs
@@ -6,7 +6,7 @@ using System.Collections;
 
     public GameObject attractedTo;
     public float strengthOfAttraction = 5.0f;
-
+    public float maxRange = 10;
     private Rigidbody2D rg2d;
     private float speed = 10.0f;
     private Vector2 target;
@@ -19,10 +19,29 @@ using System.Collections;
 
     void Update()
     {
-        Vector3 direction = attractedTo.transform.position - transform.position;
-        //rg2d.AddForce(strengthOfAttraction * direction
+        var heading = attractedTo.transform.position - transform.position;
+        if (heading.sqrMagnitude < maxRange * maxRange)
+        {
+            //rg2d.((attractedTo.transform.position - transform.position).normalized * strengthOfAttraction);
+            // The step size is equal to speed times frame time.
+            float step = speed * Time.deltaTime;
 
-        float step = speed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, direction, -step);
+            // Move our position a step closer to the target.
+            transform.position = Vector3.MoveTowards(transform.position, attractedTo.transform.position, step);
+        }
+
+
+        //Vector3 direction = attractedTo.transform.position - transform.position;
+        ////rg2d.AddForce(strengthOfAttraction * direction
+
+        //float step = speed * Time.deltaTime;
+        //transform.position = Vector2.MoveTowards(transform.position, direction, -step);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxRange);
     }
 }
